b951d9130df48b4819dedea71a8996371fee8a69
Sam Taylor <samjt775@gmail.com>
2023-08-07 19:30:00 +1000

    update to ros jazzy, fix broken pkgs

--- install.sh
+++ install.sh
@@ -9,14 +9,17 @@ sudo apt install -y software-properties-common curl wget git python3-colcon-comm
 
 # Add ROS 2 repository
 sudo apt update && sudo apt install -y curl gnupg2 lsb-release
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2.list'
+
+if [ ! -d /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+    sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
+    sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2.list'
+fi
 
 # Install latest LTS version of ROS 2 (Humble or Rolling)
-sudo apt update && sudo apt install -y ros-humble-desktop
+sudo apt update && sudo apt install -y ros-jazzy-desktop
 
 # Source ROS 2
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/humble/setup.bash" >>~/.bashrc
 source ~/.bashrc
 
 # Install ROS 2 dependencies
@@ -25,19 +28,19 @@ rosdep update
 rosdep install --from-paths src --ignore-src -r -y
 
 # Install Kinect Freenect and ROS 2 Wrapper
-sudo apt install -y ros-humble-image-pipeline ros-humble-vision-msgs ros-humble-perception-pcl
+sudo apt install -y ros-jazzy-image-pipeline ros-jazzy-vision-msgs ros-jazzy-perception-pcl
 mkdir -p ~/ros2_ws/src && cd ~/ros2_ws/src
 git clone https://github.com/fadlio/kinect_ros2.git
 cd ~/ros2_ws && colcon build --symlink-install
 
 # Install Navigation Stack for Differential Drive Robots
-sudo apt install -y ros-humble-navigation2 ros-humble-nav2-bringup ros-humble-slam-toolbox
+sudo apt install -y ros-jazzy-navigation2 ros-jazzy-nav2-bringup ros-jazzy-slam-toolbox
 
 # Install GMapping for 2D SLAM
-sudo apt install -y ros-humble-slam-gmapping
+sudo apt install -y ros-jazzy-slam-gmapping
 
 # Install Gazebo and Simulation Tools
-sudo apt install -y gazebo11 ros-humble-gazebo-ros-pkgs ros-humble-gazebo-plugins
+sudo apt install -y gazebo11 ros-jazzy-gazebo-ros-pkgs ros-jazzy-gazebo-plugins
 
 # Install YOLOv11 (assuming a compatible version is available)
 mkdir -p ~/ros2_ws/src && cd ~/ros2_ws/src
@@ -45,7 +48,7 @@ git clone https://github.com/ultralytics/yolov11.git
 cd yolov11 && pip install -r requirements.txt
 
 # Install AWS Greengrass and MQTT Client
-sudo apt install -y ros-humble-aws-iot-ros2
+sudo apt install -y ros-jazzy-aws-iot-ros2
 
 # Install Amazon Kinesis Video Streams SDK
 mkdir -p ~/ros2_ws/src && cd ~/ros2_ws/src
@@ -53,7 +56,7 @@ git clone https://github.com/aws-robotics/kinesisvideo-ros2.git
 cd ~/ros2_ws && colcon build --symlink-install
 
 # Source the workspace
-echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
+echo "source ~/ros2_ws/install/setup.bash" >>~/.bashrc
 source ~/.bashrc
 
 # Create a systemd service to launch bringup node on startup
@@ -74,6 +77,5 @@ EOF
 # Enable the service
 sudo systemctl daemon-reload
 sudo systemctl enable ros2_bringup.service
-
 
 echo "ROS 2 setup complete. Please reboot your system."
