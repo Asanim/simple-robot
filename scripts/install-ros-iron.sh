9ed62847fd156b09ee42bfb290e42d7f818261f3
Sam Taylor <samjt775@gmail.com>
2023-08-16 20:16:00 +0800

    feat: Add simple ros2 python package

--- scripts/install-ros-iron.sh
+++ scripts/install-ros-iron.sh
@@ -1,27 +1,27 @@
 #!/bin/bash -e
 
-# Check for UTF-8
-locale
+# # Check for UTF-8
+# locale
 
-# Install locales and set UTF-8
-sudo apt update && sudo apt install locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
+# # Install locales and set UTF-8
+# sudo apt update && sudo apt install locales
+# sudo locale-gen en_US en_US.UTF-8
+# sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+# export LANG=en_US.UTF-8
 
-# Verify locale settings
-locale
+# # Verify locale settings
+# locale
 
-# Install required packages
-sudo apt install -y software-properties-common
-sudo add-apt-repository universe
-sudo apt update && sudo apt install -y curl
+# # Install required packages
+# sudo apt install -y software-properties-common
+# sudo add-apt-repository universe
+# sudo apt update && sudo apt install -y curl
 
-# Download ROS keyring
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# # Download ROS keyring
+# sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-# Add ROS 2 repository to sources
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+# # Add ROS 2 repository to sources
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # Update package list and install ROS dev tools
 sudo apt update && sudo apt install -y ros-dev-tools
---
+++ src/simple_inference/simple_inference/yolov7.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from simple_inference.')
+
+
+if __name__ == '__main__':
+    main()
