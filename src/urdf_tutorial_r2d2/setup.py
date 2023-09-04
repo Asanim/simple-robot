64864e44733c064d7188d6883ad173efbec1d2a0
Sam Taylor <samjt775@gmail.com>
2023-09-04 18:44:00 +0800

    feat: Add launch script, update setup py

--- src/urdf_tutorial_r2d2/setup.py
+++ src/urdf_tutorial_r2d2/setup.py
@@ -1,4 +1,7 @@
+import os
+from glob import glob
 from setuptools import setup
+from setuptools import find_packages
 
 package_name = 'urdf_tutorial_r2d2'
 
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+          (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+  (os.path.join('share', package_name), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +25,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'state_publisher = urdf_tutorial_r2d2.state_publisher:main'
         ],
     },
+    
 )
