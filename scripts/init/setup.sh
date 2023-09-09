3a6324361491497546f22fe5a9e03df0822b5eca
Sam Taylor <samjt775@gmail.com>
2023-09-09 17:24:00 +0800

    fix: move kinect node to src

--- scripts/init/setup.sh
+++ scripts/init/setup.sh
@@ -3,7 +3,8 @@
 # /home/robot/simple-robot/scripts
 
 # sudo apt update && sudo apt dist-upgrade -y
-sudo apt install -y git openssh-server python3 python3-pip vim ros-rolling-urdf-tutorial ros-rolling-rosbag2
+sudo apt install -y git openssh-server python3 python3-pip vim ros-rolling-urdf-tutorial ros-rolling-rosbag2 \
+    libfreenect-bin libfreenect-dev \
 
 cd ~/
 git clone https://github.com/ibaiGorordo/ONNX-YOLOv7-Object-Detection.git 
