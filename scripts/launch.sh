e4e3374c098da6cb21cdbed486de0fcebbf14077
Sam Taylor <samjt775@gmail.com>
2023-09-16 19:31:01 +0800

    fix: spider urdf

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,6 @@
-source ../install/setup.bash
+source install/setup.bash
 
 cd launch
-ros2 launch test_launch.py
+# ros2 launch test_launch.py
 # ros2 launch turtlesim_mimic_launch.xml
+ros2 launch urdf_tutorial_r2d2 demo.launch.py
