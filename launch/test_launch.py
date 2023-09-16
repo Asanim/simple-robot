890c7d5521a848f77463a469164628c4a5e129e8
Sam Taylor <samjt775@gmail.com>
2023-09-16 20:33:11 +0800

    fix: merge launch files

--- launch/test_launch.py
+++ launch/test_launch.py
@@ -1,5 +1,7 @@
 import os
 
+from ament_index_python.packages import get_package_share_directory
+
 import launch_ros
 from launch_ros.actions.node import Node
 
@@ -13,9 +15,21 @@ def generate_launch_description():
         "kinect_ros2"
     )
     default_rviz_config_path = os.path.join(pkg_share, "rviz/pointcloud.rviz")
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf_file_name = 'spider.urdf.xml'
+    urdf = os.path.join(
+        get_package_share_directory('urdf_tutorial_r2d2'),
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
 
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                'use_sim_time',
+                default_value='false',
+                description='Use simulation (Gazebo) clock if true'),
+
             DeclareLaunchArgument(
                 name="rvizconfig",
                 default_value=default_rviz_config_path,
@@ -48,6 +62,18 @@ def generate_launch_description():
                 parameters=[{"window_name": "Depth"}],
                 remappings=[("image", "kinect/depth/image_raw")],
             ),
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+                arguments=[urdf]),
+            Node(
+                package='urdf_tutorial_r2d2',
+                executable='state_publisher',
+                name='state_publisher',
+                output='screen'),
 
         ]
     )
